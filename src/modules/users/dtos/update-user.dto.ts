e554536b84c865787fd6458029ea970448b679a9
Leandro Andrekowicz <103007824+LeandroAndrekowicz@users.noreply.github.com>
2024-07-22 17:16:41 -0300

    Generic Decorators para o Swagger

--- src/modules/users/dtos/update-user.dto.ts
+++ src/modules/users/dtos/update-user.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class UpdateUserDto {
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
 }
